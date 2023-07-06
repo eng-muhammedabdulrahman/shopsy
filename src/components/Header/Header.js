1a83eb7cdc4e055ea10baf0e179dc9db6315b73e
eng-muhammedabdulrahman <eng-muhammed.abdulrahman@outlook.com>
2023-07-06 10:46:54 +0300

    update

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Logo from "../assets/logo2.png";
-import SearchIcon from '@mui/icons-material/Search';
+
+import "./Header.css";
 
 const Header = () => {
   return (
@@ -11,7 +12,33 @@ const Header = () => {
       </Link>
       <div className="header-searsh">
         <input className="header-searshInput" type="text" />
-        <img src={SearchIcon} alt="searsh-icon"/>
+        <button className="header-searchIcon">
+          <i class="fa-solid fa-magnifying-glass"></i>
+        </button>
+      </div>
+      <div className="header-nav">
+        <Link to="/login">
+          <div className="header-option">
+            <div className="header-optionLineOne">Hello Guest</div>
+            <div className="header-optionLineTwo">Sign In</div>
+          </div>
+        </Link>
+        <Link to="/orders">
+          <div className="header-option">
+            <div className="header-optionLineOne">Returns</div>
+            <div className="header-optionLineTwo">& Orders</div>
+          </div>
+        </Link>
+        <div className="header-option">
+          <div className="header-optionLineOne">Your</div>
+          <div className="header-optionLineTwo">Prime</div>
+        </div>
+        <Link to="/checkout">
+          <div className="header-optionBasket">
+            //icon
+            <span className="header-optionLineTwo header-"></span>
+          </div>
+        </Link>
       </div>
     </div>
   );

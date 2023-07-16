ce537355625ed04c75e6e85a5908425151052460
eng-muhammedabdulrahman <eng-muhammed.abdulrahman@outlook.com>
2023-07-17 01:13:11 +0300

    create auth user

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import Logo from "../assets/logo2.png";
 
 import "./Header.css";
+import { Search } from "@mui/icons-material";
 
 const Header = () => {
   return (
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import Logo from "../assets/logo1.png";
 import { Link } from "react-router-dom";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebase";
 import "./Login.css";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const register = (e) => {
+    e.preventDefault();
+    createUserWithEmailAndPassword(auth , email , password);
+  };
   return (
     <div className="login">
       <Link to="/">
@@ -13,9 +21,15 @@ const Login = () => {
         <h1>Sign in</h1>
         <form>
           <h5>Email</h5>
-          <input type="email" value="" />
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
           <h5>Password</h5>
-          <input type="password" value="" />
+          <input type="password" value={password}
+          onChange={(e) => setPassword(e.target.value)}
+           />
           <button className="login-signInBtn" type="submit">
             Sign in
           </button>
@@ -23,7 +37,7 @@ const Login = () => {
             By continuing, you agree to Shopsy Online Condition of Use and
             Privacy Notice.
           </p>
-          <button className="login-registerBtn">
+          <button className="login-registerBtn" onClick={register}>
             Create your Shopsy Account
           </button>
         </form>
